2ca735a3f251ed18fc038ad2c2adf67aacd222b4
KevinYakha <kevin@yakha.eu>
2024-04-23 14:43:32 +0200

    SqueakyClean solved

--- SqueakyClean.cs
+++ SqueakyClean.cs
@@ -1,9 +1,23 @@
-using System;
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
+        identifier = identifier.Replace(' ', '_');
+        identifier = identifier.Replace("\0", "CTRL");
+        if (Regex.IsMatch(identifier, @"\S-\S"))
+        {
+            char[] identifierArray = identifier.ToCharArray();
+            char characterToCamel = identifierArray[identifier.IndexOf('-') + 1];
+            identifierArray[identifier.IndexOf('-') + 1] = characterToCamel.ToString().ToUpper().ElementAt(0);
+            identifier = new string(identifierArray);
+            identifier = identifier.Replace("-", "");
+        }
+        identifier = Regex.Replace(identifier, @"\W|\d", "");
+        identifier = Regex.Replace(identifier, @"[α-ω]", "");
+        return identifier;
     }
 }
